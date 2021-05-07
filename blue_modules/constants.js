6cfca86450567f09731c96682aa0b567e31f0484
Jenova7 <38404798+Jenova7@users.noreply.github.com>
2021-05-07 09:51:18 +0200

    update "addWallet" icons
    - temp GroundControl url

--- blue_modules/constants.js
+++ blue_modules/constants.js
@@ -2,4 +2,4 @@
  * Let's keep config vars, constants and definitions here
  */
 
-export const groundControlUri = 'https://groundcontrol-bluewallet.herokuapp.com/';
+export const groundControlUri = 'http://54.37.235.120:3001/';
